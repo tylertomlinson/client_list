2e0c3e230d3bf3a06f194fc0214db86508524a4f
Tyler Tomlinson <twtomlinson89@gmail.com>
2021-02-21 23:55:55 -0700

    Refactor to abstract for scope

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,17 @@
 class ReviewsController < ApplicationController
 
-  def index 
-    reviews = Review.where(client_id: params[:client_Id])
-    render json:  reviews
+  def index
+    reviews = Review.by_client_id(params[:client_Id])
+    render json: reviews
   end
 
   def show
+    render json: one_review([:id])
+  end
+
+  private
+
+  def one_review(id)
+    Review.find(params[:id])
   end
 end
