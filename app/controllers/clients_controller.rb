4d66637b027ac67349b0cdd2399e27c1eec11675
Tyler Tomlinson <twtomlinson89@gmail.com>
2021-02-21 23:56:39 -0700

    Refactor to include self

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,15 +1,13 @@
 class ClientsController < ApplicationController
 
   def index
-    #will return only the client names
-    names = client_obj.client_names(all_clients)
     render json: all_clients
   end
 
   def show
     id = params[:id]
     #will return active or inactive of client subscription depending on boolean statment
-    subscription_status = client_obj.subscription_status(one_client(id))
+    subscription_status = Client.subscription_status(one_client(id))
     render json: one_client(id)
   end
 
--- app/models/client.rb
+++ app/models/client.rb
@@ -1,11 +1,11 @@
 class Client < ApplicationRecord
   has_many :reviews
 
-  def client_names(clients)
+  def self.client_names(clients)
     clients.pluck :name
   end
 
-  def subscription_status(client)
+  def self.subscription_status(client)
     #this is going off how I initially set up table
     #the better way to do this might be with a default status and enum. enum status: [ :inactive, :active ]
     #t.column :status, :integer, default: 0
