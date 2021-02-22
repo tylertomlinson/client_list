47685cf1e61a8b17a2d5a244c9e803d7f87742b6
Tyler Tomlinson <twtomlinson89@gmail.com>
2021-02-21 22:58:15 -0700

    Add method to display subscription

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,3 +1,18 @@
 class Client < ApplicationRecord
   has_many :reviews
+
+  def client_names(clients)
+    clients.pluck :name
+  end
+
+  def subscription_status(client)
+    #this is going off how I initially set up table
+    #the better way to do this might be with a default status and enum. enum status: [ :inactive, :active ]
+    #t.column :status, :integer, default: 0
+    if client.subscribed? == true
+      'active'
+    else
+      'inactive'
+    end
+  end
 end
